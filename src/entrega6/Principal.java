502f352a62c1f7b0886b4313d32834df2482a1c0
Jucaruhe <126993478+Jucaruhe@users.noreply.github.com>
2023-06-29 15:27:36 -0500

    Este es el codigo principal de la interfaz

---
+++ src/entrega6/Principal.java
@@ -0,0 +1,14 @@
+
+package entrega6;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        
+        Ventanaprincipal UI = new Ventanaprincipal();
+        
+        UI.setVisible(true);
+        
+    }
+    
+}
